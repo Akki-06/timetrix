50f669e3e062a171314a365ded0b72e3e327b535
Akhil <180540953+Akki-06@users.noreply.github.com>
2026-02-18 19:25:33 +0530

    Scheduler models created

--- server/scheduler/views.py
+++ server/scheduler/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TimeSlot, Timetable, LectureAllocation
+from .serializers import (
+    TimeSlotSerializer, 
+    TimetableSerializer, 
+    LectureAllocationSerializer
+)
 
-# Create your views here.
+
+class TimeSlotViewSet(viewsets.ModelViewSet):
+    queryset = TimeSlot.objects.all()
+    serializer_class = TimeSlotSerializer
+
+class TimetableViewSet(viewsets.ModelViewSet):
+    queryset = Timetable.objects.all()
+    serializer_class = TimetableSerializer
+
+class LectureAllocationViewSet(viewsets.ModelViewSet):
+    queryset = LectureAllocation.objects.all()
+    serializer_class = LectureAllocationSerializer
